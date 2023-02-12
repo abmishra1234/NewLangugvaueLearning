556fb11ce85185fd8d4d9c4015b40470b2de74fb
Abinash Kumar Mishra <abmishra1234@gmail.com>
2023-02-12 19:22:24 +0530

    Lets start learning Csharp for your current requirementgit add .!

---
+++ day0001/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello C#");
+
+int num = 10;
+var num1 = 20;
+
+Console.WriteLine(num);
+Console.WriteLine(num1);
+
+
+const int num2 = 10;
+Console.WriteLine(num2);
+
+double myDouble = 9.78;
+int myInt = (int)myDouble;    // Manual casting: double to int
+
+Console.WriteLine(myDouble);   // Outputs 9.78
+Console.WriteLine(myInt);      // Outputs 9
+
+myInt = 10;
+myDouble = 5.25;
+bool myBool = true;
+
+Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+
+// Type your username and press enter
+//Console.WriteLine("Enter username:");
+
+//// Create a string variable and get user input from the keyboard and store it in the variable
+//string userName = Console.ReadLine();
+
+//// Print the value of the variable (userName), which will display the input value
+//Console.WriteLine("Username is: " + userName);
+
+
+//Console.WriteLine("Enter your age:");
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Your age is: " + age);
+
+int x = 3;
+int y = 3;
+Console.WriteLine(x > y); // returns True because 5 is greater than 3
+
+Console.WriteLine(Math.Max(5, 5));
+Console.WriteLine(Math.Min(5, 5));
+Console.WriteLine(Math.Sqrt(64));
+
+            // Let's test your code change
+            // Test
+
